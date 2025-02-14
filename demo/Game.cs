e5dfd400226341e0ae8fe72e5f09db61888ca929
Jared Willets <jared@willets.dev>
2025-02-14 10:55:22 -0500

    Added controller loader basics

--- demo/Game.cs
+++ demo/Game.cs
@@ -5,11 +5,9 @@ using Microsoft.Xna.Framework.Input;
 using Game.Controllers;
 using Game.Entities;
 using Microsoft.Xna.Framework;
-using Game.Graphics;
 using Game.Commands;
 using Game.Tiles;
 using demo.Game;
-using demo.Game.Commands;
 using Game.Items;
 
 namespace Game
@@ -85,20 +83,7 @@ namespace Game
 
             // Make map for keyboard controller
             Dictionary<Keys, ICommand> m = new Dictionary<Keys, ICommand>();
-            m.Add(Keys.Up, new PlayerMovementCommand(p, -1, 1));
-            m.Add(Keys.Down, new PlayerMovementCommand(p, 1, 1));
-            m.Add(Keys.Right, new PlayerMovementCommand(p, 1, 0));
-            m.Add(Keys.Left, new PlayerMovementCommand(p, -1, 0));
-            m.Add(Keys.W, new PlayerMovementCommand(p, -1, 1));
-            m.Add(Keys.S, new PlayerMovementCommand(p, 1, 1));
-            m.Add(Keys.D, new PlayerMovementCommand(p, 1, 0));
-            m.Add(Keys.A, new PlayerMovementCommand(p, -1, 0));
-            m.Add(Keys.N, new PlayerAttackCommand(p));
-            m.Add(Keys.Z, new PlayerAttackCommand(p));
-            m.Add(Keys.E, new PlayerDamageCommand(p));
-            m.Add(Keys.D0, new PlayerSwitchCommand(p, Monoko.monoko, Monoko.mkBack, Monoko.mkFront, Monoko.mkLeft, Monoko.mkRight, new Rectangle[] { Monoko.scaryDefault }, new Rectangle[] { Monoko.mkEmotionallyDamaged }));
-            m.Add(Keys.D1, new PlayerSwitchCommand(p, Madotsuki.madoSpriteSheet, Madotsuki.mdBack, Madotsuki.mdFront, Madotsuki.mdLeft, Madotsuki.mdRight, new Rectangle[] { Madotsuki.mdKnifeFRetract, Madotsuki.mdKnifeF }, new Rectangle[] { Madotsuki.mdDamaged }, new Rectangle[][] { new Rectangle[] { Madotsuki.mdKnifeF}, new Rectangle[] { Madotsuki.mdKnifeB }, new Rectangle[] {Madotsuki.mdKnifeL }, new Rectangle[] { Madotsuki.mdKnifeR} }));
-            m.Add(Keys.D2, new PlayerSwitchCommand(p, Lewa.texture, Lewa.lwB, Lewa.lwF, Lewa.lwL, Lewa.lwR, new Rectangle[] { Lewa.lwAttack[0][0] }, new Rectangle[] { Lewa.lwDamaged }, Lewa.lwAttack));
+            
             keyboard.AddCommand(m);
 
         }
---
+++ demo/Game/Controllers/ControllerLoader.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using demo.Game.Commands;
+using Game.Commands;
+using demo.Game;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game.Controllers;
+
+class ControllerLoader {
+    public static void LoadSprint2Commands(KeyboardController keyboard, Player p) {
+        Dictionary<Keys, ICommand> m = new Dictionary<Keys, ICommand>();
+        m.Add(Keys.Up, new PlayerMovementCommand(p, -1, 1));
+        m.Add(Keys.Down, new PlayerMovementCommand(p, 1, 1));
+        m.Add(Keys.Right, new PlayerMovementCommand(p, 1, 0));
+        m.Add(Keys.Left, new PlayerMovementCommand(p, -1, 0));
+        m.Add(Keys.W, new PlayerMovementCommand(p, -1, 1));
+        m.Add(Keys.S, new PlayerMovementCommand(p, 1, 1));
+        m.Add(Keys.D, new PlayerMovementCommand(p, 1, 0));
+        m.Add(Keys.A, new PlayerMovementCommand(p, -1, 0));
+        m.Add(Keys.N, new PlayerAttackCommand(p));
+        m.Add(Keys.Z, new PlayerAttackCommand(p));
+        m.Add(Keys.E, new PlayerDamageCommand(p));
+        m.Add(Keys.D0, new PlayerSwitchCommand(p, Monoko.monoko, Monoko.mkBack, Monoko.mkFront, Monoko.mkLeft, Monoko.mkRight, new Rectangle[] { Monoko.scaryDefault }, new Rectangle[] { Monoko.mkEmotionallyDamaged }));
+        m.Add(Keys.D1, new PlayerSwitchCommand(p, Madotsuki.madoSpriteSheet, Madotsuki.mdBack, Madotsuki.mdFront, Madotsuki.mdLeft, Madotsuki.mdRight, new Rectangle[] { Madotsuki.mdKnifeFRetract, Madotsuki.mdKnifeF }, new Rectangle[] { Madotsuki.mdDamaged }, new Rectangle[][] { new Rectangle[] { Madotsuki.mdKnifeF}, new Rectangle[] { Madotsuki.mdKnifeB }, new Rectangle[] {Madotsuki.mdKnifeL }, new Rectangle[] { Madotsuki.mdKnifeR} }));
+        m.Add(Keys.D2, new PlayerSwitchCommand(p, Lewa.texture, Lewa.lwB, Lewa.lwF, Lewa.lwL, Lewa.lwR, new Rectangle[] { Lewa.lwAttack[0][0] }, new Rectangle[] { Lewa.lwDamaged }, Lewa.lwAttack));
+        keyboard.AddCommand(m);
+    }
+}
