fc9e51760fe70bc4c987aa0eee1714a5f59c509a
tyfredrick <insertcleverpun6@gmail.com>
2025-02-17 10:48:47 -0500

    Removed sprite-switching functionality of 0, 1, 2 keys

--- demo/Game/Controllers/ControllerLoader.cs
+++ demo/Game/Controllers/ControllerLoader.cs
@@ -21,9 +21,9 @@ class ControllerLoader {
         m.Add(Keys.N, new PlayerAttackCommand(p));
         m.Add(Keys.Z, new PlayerAttackCommand(p));
         m.Add(Keys.E, new PlayerDamageCommand(p));
-        m.Add(Keys.D0, new PlayerSwitchCommand(p, Monoko.monoko, Monoko.mkBack, Monoko.mkFront, Monoko.mkLeft, Monoko.mkRight, new Rectangle[] { Monoko.scaryDefault }, new Rectangle[] { Monoko.mkEmotionallyDamaged }));
-        m.Add(Keys.D1, new PlayerSwitchCommand(p, Madotsuki.madoSpriteSheet, Madotsuki.mdBack, Madotsuki.mdFront, Madotsuki.mdLeft, Madotsuki.mdRight, new Rectangle[] { Madotsuki.mdKnifeFRetract, Madotsuki.mdKnifeF }, new Rectangle[] { Madotsuki.mdDamaged }, new Rectangle[][] { new Rectangle[] { Madotsuki.mdKnifeF}, new Rectangle[] { Madotsuki.mdKnifeB }, new Rectangle[] {Madotsuki.mdKnifeL }, new Rectangle[] { Madotsuki.mdKnifeR} }));
-        m.Add(Keys.D2, new PlayerSwitchCommand(p, Lewa.texture, Lewa.lwB, Lewa.lwF, Lewa.lwL, Lewa.lwR, new Rectangle[] { Lewa.lwAttack[0][0] }, new Rectangle[] { Lewa.lwDamaged }, Lewa.lwAttack));
+        m.Add(Keys.Y, new PlayerSwitchCommand(p, Monoko.monoko, Monoko.mkBack, Monoko.mkFront, Monoko.mkLeft, Monoko.mkRight, new Rectangle[] { Monoko.scaryDefault }, new Rectangle[] { Monoko.mkEmotionallyDamaged }));
+        m.Add(Keys.U, new PlayerSwitchCommand(p, Madotsuki.madoSpriteSheet, Madotsuki.mdBack, Madotsuki.mdFront, Madotsuki.mdLeft, Madotsuki.mdRight, new Rectangle[] { Madotsuki.mdKnifeFRetract, Madotsuki.mdKnifeF }, new Rectangle[] { Madotsuki.mdDamaged }, new Rectangle[][] { new Rectangle[] { Madotsuki.mdKnifeF}, new Rectangle[] { Madotsuki.mdKnifeB }, new Rectangle[] {Madotsuki.mdKnifeL }, new Rectangle[] { Madotsuki.mdKnifeR} }));
+        m.Add(Keys.I, new PlayerSwitchCommand(p, Lewa.texture, Lewa.lwB, Lewa.lwF, Lewa.lwL, Lewa.lwR, new Rectangle[] { Lewa.lwAttack[0][0] }, new Rectangle[] { Lewa.lwDamaged }, Lewa.lwAttack));
         m.Add(Keys.O, new EnemySwitchCommand(0));
         m.Add(Keys.P, new EnemySwitchCommand(1));
         keyboard.AddCommand(m);
