86a482befa4f685a10999e96b15820518ac61426
Jared Willets <jared@willets.dev>
2025-03-18 15:55:41 -0400

    added UI button basic press detection

--- Game/Graphics/UIButton.cs
+++ Game/Graphics/UIButton.cs
@@ -9,13 +9,24 @@ public class UIButton : IUserInterfaceElement {
     private Rectangle bounds;
     private MouseController mc;
     private ICommand onPress;
+    private bool pressed = false;
     public UIButton(Rectangle bounds, MouseController mc, ICommand onPress) {
         this.bounds = bounds;
         this.mc = mc;
         this.onPress = onPress;
     }
-    public void Update(GameTime gameTime) {}
+    public void Update(GameTime gameTime) {
+        if (
+            bounds.Left <= mc.PositionX() && mc.PositionX() <= bounds.Right &&
+            bounds.Top <= mc.PositionY() && mc.PositionY() <= bounds.Bottom && mc.LeftDown() && !pressed
+        ) {
+            onPress.Execute();
+        }
+        if (!mc.LeftDown()) {
+            pressed = false;
+        }
+    }
     public void Draw(SpriteBatch spriteBatch) {
-
+        
     }
 }
