c82a4c2378529bca6231fae60ddb1ae7a83dae5d
Jared Willets <jared@willets.dev>
2025-04-21 04:42:24 -0400

    Finished basics for credits page

--- Game/Graphics/Layouts/UICreditLayout.cs
+++ Game/Graphics/Layouts/UICreditLayout.cs
@@ -1,5 +1,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Game.Commands;
+using Game.State;
+using System.Collections.Generic;
 
 namespace Game.Graphics;
 
@@ -20,7 +23,25 @@ public class UICreditLayout : UILayout {
         header.SetOutline(textColor);
         AddElement(header);
 
+        // Add Names
+        string[] names = ["Erich Sullivan Boschert","Mingyu Choi","Ty Tram Fredrick","Jared Willets"];
+        int c = 0;
+        foreach (string name in names) {
+            Vector2 namePos = new(
+                w/2 - Main.fonts["header"].MeasureString(name).X/2,
+                h/4 + 48*(c+1) + 24
+            );
+            UIText currentName = new UIText(name, namePos, "header", Color.AntiqueWhite);
+            currentName.SetOutline(Color.Black);
+            AddElement(currentName);
+            c++;
+        }
+
+
         // Add Back Button
-        
+        Rectangle backButtonBounds = new((int)(0.05*w), (int)(0.9*h),(int)(0.1*w), (int)(0.05*h));
+        UITextButton backButton = new UITextButton(backButtonBounds, Main.INSTANCE.mouse, new MenuCommand(), buttonColor, "Back", Color.Black, "arialbold");
+        backButton.SetHoverColor(ColorTransform.Add(buttonColor, -20,-20,-20));
+        AddElement(backButton);
     }
 }
